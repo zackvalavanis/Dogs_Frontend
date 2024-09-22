310916307c66535b297fe8edd3ce99790d25acfb
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-22 17:49:38 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { DogsPage } from "./DogsPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <DogsPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/DogsPage.jsx
@@ -0,0 +1,7 @@
+export function DogsPage() {
+  return (
+    <main>
+      <h1>Welcome to React!</h1>
+    </main>
+  );
+}
